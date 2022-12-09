7c9f779e70a6a9540cfbdfda86624976225051dd
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-09 18:35:49 +0300

    Task №10

---
+++ Task/Program.cs
@@ -0,0 +1,41 @@
+﻿// Task 10 
+// Принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+ 
+bool NumberInInterval(int a, int b, int c)
+{
+    if (a >= b && a <= c)
+        return true;
+    else 
+    {
+        Console.WriteLine($"Ошибка! Число должно находиться в интервале от {b} до {c}!");
+        return false;
+    }   
+}
+
+int n;
+Console.Clear();
+do
+{
+    Console.Write("Введите трехзначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+} while (!NumberInInterval(n, 100, 999));
+
+int m = n / 10 - (n / 100) * 10;
+Console.WriteLine($"Вторая цифра числа: {m}.");
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
